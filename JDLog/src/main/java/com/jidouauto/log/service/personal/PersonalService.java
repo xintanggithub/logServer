7680d14d6eec162eb45bf24c3c6183a79b4df544
tangxin <tsontang@jidouauto.com>
2018-11-23 17:58:03 +0800

    utf-8

--- JDLog/src/main/java/com/jidouauto/log/service/personal/PersonalService.java
+++
@@ -1,8 +0,0 @@
-package com.jidouauto.log.service.personal;
-
-import com.github.pagehelper.PageInfo;
-import com.jidouauto.log.model.PersonalEntity;
-
-public interface PersonalService {
-    PageInfo<PersonalEntity> getAll(int pageNum, int pageSize);
-}
