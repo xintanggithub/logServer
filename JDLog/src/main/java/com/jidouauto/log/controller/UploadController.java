c7c4076e66ba787bbc0f738293a89ab8dee85844
tangxin <tsontang@jidouauto.com>
2018-12-05 18:09:21 +0800

    路径配置

--- JDLog/src/main/java/com/jidouauto/log/controller/UploadController.java
+++ JDLog/src/main/java/com/jidouauto/log/controller/UploadController.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,9 @@ public class UploadController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UploadController.class);
 
+    @Value("${market.log.file.path}")
+    private String path;
+
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
     @ApiOperation(value = "上传文件", notes = "提供各种文件上传服务")
     public BaseResponse<String> uploadFile(@RequestParam(value = "file", required = true) MultipartFile file) {
@@ -30,10 +34,12 @@ public class UploadController {
             response.setResultMessage(LogCode.RC_UPLOAD_FILE_ERROR.getMessage());
             return response;
         }
-        String contentType = file.getContentType();   //图片文件类型
-        String fileName = file.getOriginalFilename();  //图片名字
+        String contentType = file.getContentType();   //文件类型
+        String fileName = file.getOriginalFilename();  //名字
+        LOGGER.info("文件类型:" + contentType + "    文件名称:" + fileName);
         //文件存放路径
-        String filePath = "/Users/tangxin/work/springCloud/serviceAddress/";
+        String filePath = path;
+        LOGGER.info("存放路径:" + path);
         //调用文件处理类FileUtil，处理文件，将文件写入指定位置
         try {
             FileUtil.uploadFile(file.getBytes(), filePath, fileName);
